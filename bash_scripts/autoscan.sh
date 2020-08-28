4bc1e3bf15aaa51bc9dd0ffa51aa762d7a8d9a42
sbamsbam <sbam@protonmail.com>
2020-08-28 19:59:32 +0200

    v 1.0

--- bash_scripts/autoscan.sh
+++ bash_scripts/autoscan.sh
@@ -9,15 +9,16 @@ cat << "EOF"
 #+#     #+# #+#    #+#     #+#    #+#    #+# #+#    #+# #+#    #+# #+#     #+# #+#   #+#+# 
 ###     ###  ########      ###     ########   ########   ########  ###     ### ###    #### 
 EOF
-
+echo 
+echo
 read -p "Inserisci la cartella dove vuoi salvare i risultati " PATH
 if [ ! -d $PATH ]; then
-  mkdir -p $PATH;
+  /usr/bin/mkdir -p $PATH;
 fi
 
 read -p "Inserisci il CIDR della rete che vuoi scansionare: " CIDR
 
-nmap -n -sn $CIDR -oG - | awk '/Up$/{print $2}' > $PATH/$CIDR.txt &&
+/usr/bin/nmap -n -sn $CIDR -oG - | awk '/Up$/{print $2}' > $PATH/$CIDR.txt &&
 
 /usr/bin/python3 /AutoRecon/src/autorecon/autorecon.py -t $CIDR -o $PATH         
 
