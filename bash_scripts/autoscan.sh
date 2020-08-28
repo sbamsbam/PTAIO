a50eae170e0dec504a036f50fdb7f2affaba79ee
sbamsbam <sbam@protonmail.com>
2020-08-28 20:04:53 +0200

    v 1.0

--- bash_scripts/autoscan.sh
+++ bash_scripts/autoscan.sh
@@ -15,10 +15,10 @@ read -p "Inserisci la cartella dove vuoi salvare i risultati " PATH
 if [ ! -d $PATH ]; then
   /usr/bin/mkdir -p $PATH;
 fi
-
+read -p "Inserisci il nome che vuoi dare al progetto: " PROJECT
 read -p "Inserisci il CIDR della rete che vuoi scansionare: " CIDR
 
-/usr/bin/nmap -n -sn $CIDR -oG - | awk '/Up$/{print $2}' > $PATH/$CIDR.txt &&
+/usr/bin/nmap -n -sn $CIDR -oG - | awk '/Up$/{print $2}' > $PATH/$PROJECT.txt &&
 
-/usr/bin/python3 /AutoRecon/src/autorecon/autorecon.py -t $CIDR -o $PATH         
+/usr/bin/python3 /AutoRecon/src/autorecon/autorecon.py -t $CIDR -o $PATH/$PROJECT.txt 
 
