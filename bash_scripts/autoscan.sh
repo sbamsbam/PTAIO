e9b13ade19afe9debfd30f6f7bdbe4ff932eec21
sbamsbam <sbam@protonmail.com>
2020-08-29 10:41:45 +0200

    v 1.1

--- bash_scripts/autoscan.sh
+++ bash_scripts/autoscan.sh
@@ -11,14 +11,43 @@ cat << "EOF"
 EOF
 echo 
 echo
-read -p "Inserisci la cartella dove vuoi salvare i risultati " PATH
-if [ ! -d $PATH ]; then
-  /usr/bin/mkdir -p $PATH;
-fi
+START()
+{
 read -p "Inserisci il nome che vuoi dare al progetto: " PROJECT
-read -p "Inserisci il CIDR della rete che vuoi scansionare: " CIDR
+read -p "Inserisci la cartella dove vuoi salvare i risultati: " PATH 
+if [ ! -d $PATH$PROJECT ]; then
+  /usr/bin/mkdir -p $PATH$PROJECT;
+fi
 
-/usr/bin/nmap -n -sn $CIDR -oG - | /usr/bin/awk '/Up$/{print $2}' > $PATH/$PROJECT.txt &&
+}
 
-/usr/bin/python3 /AutoRecon/src/autorecon/autorecon.py -t $CIDR -o $PATH/$PROJECT.txt 
+SCAN()
+{
+read -p  "Inserisci il CIDR della rete che vuoi scansionare: " CIDR
 
+/usr/bin/nmap -n -sn $CIDR -oG - | /usr/bin/awk '/Up$/{print $2}' >> $PATH/$PROJECT/$PROJECT.txt &&
+
+/usr/bin/python3 /root/AutoRecon/src/autorecon/autorecon.py -t $PATH/$PROJECT/$PROJECT.txt -o $PATH/$PROJECT/
+
+echo "Scansione eseguita, i file sono stati salvati in $PATH/$PROJECT/"
+}
+START
+SCAN
+echo
+while true; do
+    read -p  "Vuoi scansionare una nuova rete? (S/N) " REPLY
+    case $REPLY in
+        [Nn]* ) exit;;
+        
+        [Ss]* ) while true; do
+        		        read -p  "Vuoi utilizzare lo stesso progetto? (S/N) " REPLY2
+        		        case $REPLY2 in
+        			        [Ss]* ) SCAN;;
+        			        [Nn]* ) START;;
+                      * ) echo "Rispondi Si o No";;
+                    esac
+                done
+        #* ) echo "Rispondi Si o No";;
+    esac
+done
+exit
